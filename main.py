42d625370fc6b143c7b8d37816daf03c1ec18e44
VarRoman <bezshchasnyiroman7a76@gmail.com>
2024-06-01 19:03:41 +0200

    Standart commit 3

--- main.py
+++ main.py
@@ -35,24 +35,58 @@ class PlayerPlace(BoxLayout):
         super().__init__(**kwargs)
 
 
+class EllipseWidget(Widget):
+    def __init__(self, color, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Color(*color)
+            self.ellipse = Ellipse(size=(60, 60))
+        self.bind(pos=self.update_ellipse, size=self.update_ellipse)
+
+    def update_ellipse(self, *args):
+        # Оновлення позиції еліпса, щоб він був в центрі віджета
+        self.ellipse.pos = (self.x + 40, self.y + 60)  # Послідовно оновлюємо позицію еліпса на основі положення віджета
+
+
 class GamePlace(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        print(self.size)
+        self.orientation = 'vertical'
 
-        with self.canvas:
-            x = 135
-            y = 0
-            for i in range(12):
-                Color(0, 123, 441, 1)
-                Ellipse(pos=(x, y), size=(80, 80), )
-                x += 80
 
-            y = 285
-            x = 135
-            for i in range(12):
-                Color(0, 1, 0, 1)
-                Ellipse(pos=(x, y), size=(80, 80), )
-                x += 80
+
+        # Верхній макет
+        top = AnchorLayout(anchor_y='bottom')
+        top_up = BoxLayout(orientation='horizontal')
+        top.add_widget(top_up)
+        # top.add_widget(Button(text='gjfldgj', size_hint=(0.3, 0.3)))
+
+        # Додавання верхніх еліпсів
+        for i in range(12):
+            el = EllipseWidget(color=(94/255, 80/255, 61/255, 1))
+            top_up.add_widget(el)
+
+        self.add_widget(top)
+
+        center = AnchorLayout(anchor_y='center')
+        center.add_widget(Label(text='                               ', size_hint=(1, 1)))
+        self.add_widget(center)
+        center1 = AnchorLayout(anchor_y='center')
+        center1.add_widget(Label(text='                               ', size_hint=(1, 1)))
+        self.add_widget(center1)
+
+        # Нижній макет
+        bottom = AnchorLayout(anchor_y='bottom')
+        bottom_down = BoxLayout(orientation='horizontal')
+        bottom.add_widget(bottom_down)
+
+        # Додавання нижніх еліпсів
+        for i in range(12):
+            el = EllipseWidget(color=(1, 1, .85, 1))
+            bottom_down.add_widget(el)
+
+        self.add_widget(bottom)
 
 
 
